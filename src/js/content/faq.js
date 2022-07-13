bfd6c480dc842697fe6794f06c9f9900033a9dd2
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-07-13 11:52:24 +0300

    feat: add some animation to blocks

--- src/js/content/faq.js
+++ src/js/content/faq.js
@@ -3,7 +3,8 @@ export const showText = () => {
     let itemsText = document.querySelectorAll('ul>li>p')
     if(itemsText.length >= 1){
         itemsText.forEach(item => {
-            if(item.offsetHeight > 132){
+            if(item.offsetHeight > 154){
+                let itemHeight = item.offsetHeight + 'px';
                 let parentOfItem= item.parentNode;
                 parentOfItem.classList.add('show-text');
                 let showMore = document.createElement('span');
@@ -12,8 +13,11 @@ export const showText = () => {
                 showMore.onclick = () => {
                     let activeItem = document.querySelector('li.active');
                     parentOfItem.classList.add('active');
+                    item.style.maxHeight = itemHeight;
                     parentOfItem.classList.remove('show-text')
                     if(activeItem){
+                        let activeItemStyle = activeItem.querySelector('p');
+                        activeItemStyle.style.maxHeight = '';
                         activeItem.classList.remove('active')
                         activeItem.classList.add('show-text')
                     }
