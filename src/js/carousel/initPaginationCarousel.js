0d227560781b92a3500e0868f866f5bc21287426
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-07-20 15:16:02 +0300

    feat: finishing how-to-use slider-block new design

--- src/js/carousel/initPaginationCarousel.js
+++ src/js/carousel/initPaginationCarousel.js
@@ -1,53 +1,53 @@
 import Splide from "@splidejs/splide";
 
 export const initPaginationCarousel = () => {
-    const reviewsCarousel = document.querySelector('.pagination-carousel');
-    if (!!reviewsCarousel) {
-        const carousel = new Splide('.pagination-carousel', {
-            perPage: 3,
-            perMove: 3,
-            gap: 32,
-            arrows: true,
-            speed: 800,
-            easing: 'ease',
-            updateOnMove: true,
-            pauseOnHover: false,
-            resetProgress: false,
-            breakpoints: {
-                991: {
-                    autoWidth: true,
-                    perPage: 1,
-                    perMove: 1,
-                    focus: 'center',
-                },
-                767: {
-                    gap: 15,
-                    autoWidth: true,
-                    perPage: 1,
-                    perMove: 1,
-                    focus: 'center',
-                },
-            },
-        });
+    const reviewsCarousels = document.querySelectorAll('.pagination-carousel');
+    if (reviewsCarousels.length > 0) {
+        for(let i = 0; i < reviewsCarousels.length; i++ ) {
+            function paginationFraction( data ) {
+                data.list.classList.add( 'pagination-fraction' );
+                let arrows = reviewsCarousels[i].querySelectorAll('.splide__arrow');
+                let totalValue = reviewsCarousels[i].querySelector('.splide__fractions-total');
+                let currentValue = reviewsCarousels[i].querySelector('.splide__fractions-current');
+                totalValue.textContent = `${data.items.length}`;
+                currentValue.textContent = '1';
 
-        carousel .on( 'pagination:mounted', function ( data ) {
-            data.list.classList.add( 'pagination-fraction' );
-            let arrows = document.querySelectorAll('.splide__arrow');
-            let totalValue = document.querySelector('.splide__fractions-total');
-            let currentValue = document.querySelector('.splide__fractions-current');
-            totalValue.textContent = `${data.items.length}`;
-            currentValue.textContent = '1';
-
-            for (let arrow of arrows){
-                arrow.onclick = () => {
-                    const currentPage = data.items.findIndex(object => {
-                        return object.button.ariaCurrent === 'true';
-                    });
-                    currentValue.textContent = String(currentPage + 1);
+                for (let arrow of arrows){
+                    arrow.onclick = () => {
+                        const currentPage = data.items.findIndex(object => {
+                            return object.button.ariaCurrent === 'true';
+                        });
+                        currentValue.textContent = String(currentPage + 1);
+                    }
                 }
             }
-        } );
+            new Splide(reviewsCarousels[i], {
+                perPage: 3,
+                perMove: 3,
+                gap: 32,
+                arrows: true,
+                speed: 800,
+                easing: 'ease',
+                updateOnMove: true,
+                pauseOnHover: false,
+                resetProgress: false,
+                drag: false,
+                breakpoints: {
+                    991: {
+                        perPage: 2,
+                        perMove: 2,
+                    },
+                    767: {
+                        gap: 15,
+                        perPage: 1,
+                        perMove: 1,
+                    },
+                },
+            })
+                .on( 'pagination:mounted', function (data) {
+                paginationFraction( data )
+            } ).mount()
+        }
 
-        carousel.mount();
     }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -43,7 +43,6 @@ initReviewsCarousel();
 initCompareCarousel();
 initReviewsCommentsCarousel();
 initPaginationCarousel();
-
 /*
 Content
  */
