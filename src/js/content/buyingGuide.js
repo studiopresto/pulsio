ac6dba8fe918ee4b921c15a9e758f2a0fdf8922e
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-07-25 11:53:23 +0300

    fix: js orderItem

--- src/js/content/buyingGuide.js
+++ src/js/content/buyingGuide.js
@@ -1,13 +1,16 @@
 
 export const orderItems = () => {
-    let doubleColumnSection =document.querySelector('.delivery.double-column');
-    if(doubleColumnSection){
-        let listItems = doubleColumnSection.querySelectorAll('ul>li');
-        for( let i = 0; i <= listItems.length; i++){
-            listItems[i].style.order = `${i + 1}`;
-            if(listItems[i].classList.contains('mobile-order')){
-                listItems[i].style.order = `${i + 3}`;
+    if (window.screen.width <= 991){
+        let doubleColumnSection =document.querySelector('.delivery.double-column');
+        if(doubleColumnSection){
+            let listItems = doubleColumnSection.querySelectorAll('ul>li');
+            for( let i = 0; i <= listItems.length; i++){
+                listItems[i].style.order = `${i + 1}`;
+                if(listItems[i].classList.contains('mobile-order')){
+                    listItems[i].style.order = `${i + 3}`;
+                }
             }
         }
     }
+
 }
